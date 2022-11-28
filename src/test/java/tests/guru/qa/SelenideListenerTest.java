d978d122db4b0b74b075451de550abd9116f7139
d.kostenko <kdm.tag@gmail.com>
2022-11-28 23:52:53 +0300

    initial commit with SelenideListenerTest

---
+++ src/test/java/tests/guru/qa/SelenideListenerTest.java
@@ -0,0 +1,29 @@
+package tests.guru.qa;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static org.openqa.selenium.By.linkText;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideListenerTest {
+
+    @Test
+    public void checkIssueName() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com/");
+
+        $(".header-search-input").click();
+        $(".header-search-input").setValue("eroshenkoam/allure-example");
+        $(".header-search-input").pressEnter();
+
+        $(linkText("eroshenkoam/allure-example")).click();
+        $("#issues-tab").click();
+        $("#issue_81_link").shouldHave(Condition.text("issue_to_test_allure_report"));
+    }
+}
